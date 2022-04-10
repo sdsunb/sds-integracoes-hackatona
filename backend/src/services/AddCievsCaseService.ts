f1f7a8a3bcfab1ae5d9c963265d639065db3cabd
Luis Bruno <l.brunofidelis@gmail.com>
2022-04-10 20:16:30 -0300

    Updating AddCievsCaseService
    This commit adds on file the business rule and request (POST) to API

--- backend/src/services/AddCievsCaseService.ts
+++ backend/src/services/AddCievsCaseService.ts
@@ -1,4 +1,9 @@
-
+import axios from "axios";
+import { Classification, CriterioConfirmacao, defaultCase, DocumentType, FormaTransmissao, ICievsCase, OutcomeId, PregnancyStatus, RacaCor } from "../interfaces/CievsCaseInterface";
+import { getSpreadsheetPath } from "../utils/GetSpreadsheetPath";
+import readXlsxFile from "read-excel-file/node";
+import { getDate } from "../utils/StringToDate";
+import { Gender } from "../interfaces/CaseInterface";
 
 interface IRequestData {
     apiAddress: string,
@@ -161,3 +166,217 @@ const schema = {
         type: String
     }
 }
+
+class AddCievsCaseService {
+    async execute(requestData: IRequestData) {
+        const headers = {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Access-Control-Allow-Origin': "*"
+        }
+
+        let newCase: ICievsCase = defaultCase;
+        let requestResult: IResult = {
+            status: 'SUCESSO',
+            casesAdded: 0,
+            casesFail: {
+                quantity:0
+            }
+        }
+
+        try {
+            const file = await getSpreadsheetPath();
+            await readXlsxFile(file, { schema }).then(async ({ rows, errors }) => {
+                console.log("PLANILHA EM JSON:", rows);
+
+                await rows.map(async (col: any, index: number) => {
+                    newCase.visualId = col.visualId;
+                    newCase.firstName = col.firstName;
+                    newCase.gender = getGender(col.gender);
+                    newCase.outcomeId = getOutcome(col.outcomeId);
+                    newCase.dob = getDate(col.dob);
+                    
+                    newCase.outbreakId = process.env.OUTBREAK_ID;
+                    newCase.dateOfReporting = getDate(col.dateOfReporting);
+                    newCase.dateOfOnset = getDate(col.dateOfOnset);
+
+                    if(col.dataAltaMedica) {
+                        newCase.dateOfOutcome = getDate(col.dataAltaMedica);
+                    } else if(col.dataObito) {
+                        newCase.dateOfOutcome = getDate(col.dataObito);
+                    }
+
+                    // Classification in CIEVS always is CONFIRMED by default
+                    // newCase.classification = getClassification(col.classification);
+                    
+                    // addresses
+                    newCase.addresses[0].phoneNumber = col.phoneNumber;
+                    newCase.addresses[0].addressLine1 = col.addressLine1;
+                    newCase.addresses[0].postalCode = col.postalCode;
+                    // newCase.addresses[0].locationId = 'dd2fbc8c-3717-47b3-ae20-45c9da6c7876';
+
+                    // newCase.createdBy = fill this, it'll relevant
+
+                    // if has CPF, fills document type and document number
+                    // if(col.number) {
+                    //     newCase.documents[0].type = DocumentType.Cpf;
+                    //     newCase.documents[0].number = col.number;
+                    // }
+
+                    // questionnaire Answers
+                    if(col.telefone_fixo) {
+                        newCase.questionnaireAnswers.outros_telefones[0].value = "1";    // "1" is the value to "Sim" in questionnaire answers
+                        newCase.questionnaireAnswers.telefone_fixo[0].value = col.telefone_fixo;
+                    } else {
+                        newCase.questionnaireAnswers.outros_telefones[0].value = "2";   // "2" is the value to "Não" in questionnaire answers
+                    }
+
+                    newCase.questionnaireAnswers.criterio_confirmacao[0].value = getCriterioConfirmacao(col.criterio_confirmacao);
+                    newCase.questionnaireAnswers.forma_de_transmissao[0].value = getFormaTransmissao(col.forma_de_transmissao);
+                    newCase.questionnaireAnswers.raca_cor[0].value = getRacaCor(col.raca_cor);
+                    newCase.questionnaireAnswers.nome_da_mae[0].value = col.nome_da_mae;
+
+                    if(col.pneumopatia || col.Nefropatia || col.doencaHematologica || col.disturbiosMetabolicos || col.imunopressao || col.Obesidade || col.cardioVasculopatia || col.outros) {
+                        newCase.questionnaireAnswers.comorbidades[0].value = "1"    // "1" is the value to "Sim" in questionnaire answers
+                        newCase.questionnaireAnswers.qual[0].value = getComorbidades(col);
+                    } else {
+                        newCase.questionnaireAnswers.comorbidades[0].value = "2"    // "2" is the value to "Não" in questionnaire answers
+                    }
+
+                    // newCase.createdBy = '928b8d14-b3cd-4af2-80bc-de17ed862a58';
+                    // newCase.updatedBy = '928b8d14-b3cd-4af2-80bc-de17ed862a58';
+                    // newCase.updatedAt = new Date();
+                    
+
+
+                    try {
+                        var response = await axios.post(requestData.apiAddress + requestData.route, newCase, { headers });
+                        console.log(response);
+                        requestResult.casesAdded += 1;
+                        // console.log("Adicionando caso", index, newCase);
+                    } catch(error) {
+                        console.log("Deu ruim", requestResult);
+                        requestResult.casesFail.quantity += 1;
+                        console.error(error);
+                        throw new Error(error);
+                    }
+                });
+            });
+
+            return newCase;
+
+        } catch(error) {
+            console.log(error);
+            throw new Error(error);
+        }
+    }
+}
+
+function getOutcome(outcome: string) {
+    if(outcome === 'Cura') {
+        return OutcomeId.Recovered;
+    } else if(outcome === 'Óbito') {
+        return OutcomeId.Deceased;
+    } else {
+        return OutcomeId.Empty;
+    }
+}
+
+function getClassification(classification: string) {
+    if(classification === 'Confirmados') {
+        return Classification.Confirmed;
+    } else {
+        return Classification.Suspect;
+    }
+}
+
+function getGender(gender: string) {
+    if(gender === 'Masculino') {
+        return Gender.Male;
+    } else if (gender === 'Feminino') {
+        return Gender.Feminine;
+    } else {
+        return Gender.Empty;
+    }
+}
+
+function getCriterioConfirmacao(criterio_confirmacao: string) {
+    if(criterio_confirmacao === '1-Clínico Epidemiológico') {
+        return CriterioConfirmacao.ClinicoEpidemiologico;
+    } else if(criterio_confirmacao === '2-Clínico Imagem') {
+        return CriterioConfirmacao.ClinicoImagem;
+    } else if(criterio_confirmacao === '3-Confimado Por Critério Clínico') {
+        return CriterioConfirmacao.ConfirmadoCriterioClinico;
+    } else if(criterio_confirmacao === '4-Laboratorial') {
+        return CriterioConfirmacao.Laboratorial;
+    } else {
+        return "";
+    }
+}
+
+function getFormaTransmissao(forma_de_transmissao: string) {
+    let formaTransmissao: Array<String> = [];
+    if(forma_de_transmissao === 'Local') {
+        formaTransmissao.push(FormaTransmissao.Local);
+    } else if(forma_de_transmissao === 'Comunitária') {
+        formaTransmissao.push(FormaTransmissao.Comunitaria);
+    } else if(forma_de_transmissao === 'Importada') {
+        formaTransmissao.push(FormaTransmissao.Importada);
+    } else {
+        formaTransmissao.push(FormaTransmissao.Empty);
+    }
+
+    return formaTransmissao;
+}
+
+function getRacaCor(racaCor: string) {
+    if(racaCor === 'Branca') {
+        return RacaCor.Branca;
+    } else if(racaCor === 'Preta') {
+        return RacaCor.Preta;
+    } else if(racaCor === 'Amarela') { 
+        return RacaCor.Amarela;
+    } else if(racaCor === 'Parda') {
+        return RacaCor.Parda;
+    } else if(racaCor === 'Indígena') {
+        return RacaCor.Indigena;
+    } else if(racaCor === 'Sem declaração') {
+        return RacaCor.SemDeclaracao;
+    } else { 
+        return RacaCor.Empty;
+    }
+}
+
+function getComorbidades(row) {
+    let comorbidades: Array<string> = [];
+    if(row.pneumopatia === 'Sim') {
+        comorbidades.push("1");
+    }
+
+    if(row.Nefropatia === 'Sim') {
+        comorbidades.push("2");
+    }
+
+    if(row.doencaHematologica === 'Sim') {
+        comorbidades.push("3");
+    }
+
+    if(row.disturbiosMetabolicos === 'Sim') {
+        comorbidades.push("4");
+    }
+
+    if(row.imunopressao === 'Sim') {
+        comorbidades.push("5");
+    }
+
+    if(row.Obesidade === 'Sim') {
+        comorbidades.push("6");
+    }
+
+    if(row.cardioVasculopatia === 'Sim') {
+        comorbidades.push("7");
+    }
+
+    return comorbidades;
+}
+
+export { AddCievsCaseService };
